29157569f25b017f0b8e8530b7c9e36d0a3c0b0d
uzairwebdeveloper <aslam.uzair456@gmail.com>
2020-07-07 16:55:37 -0500

    updated product directory with new laptop data and added react router dom

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -3,6 +3,14 @@ import React, { Component } from "react";
 export default class product extends Component {
   render() {
     const { id, title, img, price, inCart } = this.props.product;
-    return <div></div>;
+    return <div  className="col-9 mx-auto col-md-6 col-lg-3 my-3" >
+        <div className="card">
+<div className="img-container p-5">
+    
+</div>
+
+        </div>
+
+    </div>;
   }
 }
